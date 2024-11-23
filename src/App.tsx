232726119cb46e8e61e0b4c4bed7a76ce7e90a6f
Kaio <marques.kaio16@gmail.com>
2024-11-22 21:24:24 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React dasdasasdasdaaaaa
+          Learn React dasdasasdasdaaaaattttt
         </a>
       </header>
     </div>
